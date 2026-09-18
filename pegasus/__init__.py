8ced028fa8f44011c2ac37e10b3f9f9c4a83a825
tonybaylis81-gif <tonybaylis81@gmail.com>
2026-09-19 01:47:38 +0300

    PEGASUS: make pegasus/__init__.py portable

--- pegasus/__init__.py
+++ pegasus/__init__.py
@@ -1,3 +1,9 @@
 from .core import Pegasus
+from .portable import backup_portable, bootstrap_from_portable, initialize_portable_root
 
-__all__ = ["Pegasus"]
+__all__ = [
+    "Pegasus",
+    "backup_portable",
+    "bootstrap_from_portable",
+    "initialize_portable_root",
+]
